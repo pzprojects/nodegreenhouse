a6b81e121bb57c9d53faf3f3d9b5fac31f85a204
Liron Shenhav <liron@pzprojects.local>
2020-03-03 16:13:15 +0200

    fix 2

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI:'mongodb+srv://Liron:Pz12321@greenhousedb-4sq07.mongodb.net/items?retryWrites=true&w=majority'
-}
--- routes/api/items.js
+++ routes/api/items.js
@@ -5,33 +5,63 @@ const router = express.Router();
 // Item Model
 const Item = require('../../models/item');
 
-// @route   GET api/items
-// @desc    Get All Items
-// @access  Public
-router.get('/', (req, res) => {
-  Item.find()
-    .sort({ date: -1 })
-    .then(items => res.json(items));
+/**
+ * @route   GET api/items
+ * @desc    Get All Items
+ * @access  Public
+ */
+
+router.get('/', async (req, res) => {
+  try {
+    const items = await Item.find();
+    if (!items) throw Error('No items');
+
+    res.status(200).json(items);
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
 });
 
-// @route   POST api/items
-// @desc    Create An Item
-// @access  Private
-router.post('/', (req, res) => {
+/**
+ * @route   POST api/items
+ * @desc    Create An Item
+ * @access  Private
+ */
+
+router.post('/', async (req, res) => {
   const newItem = new Item({
     name: req.body.name
   });
 
-  newItem.save().then(item => res.json(item));
+  try {
+    const item = await newItem.save();
+    if (!item) throw Error('Something went wrong saving the item');
+
+    res.status(200).json(item);
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
 });
 
-// @route   DELETE api/items/:id
-// @desc    Delete A Item
-// @access  Private
-router.delete('/:id', (req, res) => {
-  Item.findById(req.params.id)
-    .then(item => item.remove().then(() => res.json({ success: true })))
-    .catch(err => res.status(404).json({ success: false }));
+/**
+ * @route   DELETE api/items/:id
+ * @desc    Delete A Item
+ * @access  Private
+ */
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    if (!item) throw Error('No item found');
+
+    const removed = await item.remove();
+    if (!removed)
+      throw Error('Something went wrong while trying to delete the item');
+
+    res.status(200).json({ success: true });
+  } catch (error) {
+    res.status(400).json({ msg: e.message, success: false });
+  }
 });
 
 module.exports = router;
