8f68d7a5eb6cb2cb3d0739e10bc5094c87bfb8dd
Liron Shenhav <liron@pzprojects.local>
2020-05-14 12:19:10 +0300

    test

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.get("/json", (req, res) => {
  });
 //app.use(express.static(path.join(__dirname, 'client/build')));
 
+console.log(rocess.env.DB);
 // Connect to Mongo
 mongoose
   .connect(process.env.DB, { 
